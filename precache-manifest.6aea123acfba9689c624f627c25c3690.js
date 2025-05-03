282bec5e06952ddea6398bf1e0e63ed802c96144
Andy <andytony56791@gmail.com>
2025-05-03 00:56:35 -0700

    Updates

---
+++ precache-manifest.6aea123acfba9689c624f627c25c3690.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be44dab08928473da0fe811d01e6e53f",
+    "url": "/TEMPURA/index.html"
+  },
+  {
+    "revision": "3d911246412e75f76c05",
+    "url": "/TEMPURA/static/css/main.e96f071d.chunk.css"
+  },
+  {
+    "revision": "2af8c9af05ce878e0b28",
+    "url": "/TEMPURA/static/js/2.25b2e0fd.chunk.js"
+  },
+  {
+    "revision": "2984d7dc8ce4c59bfce177dbd6f36e9c",
+    "url": "/TEMPURA/static/js/2.25b2e0fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d911246412e75f76c05",
+    "url": "/TEMPURA/static/js/main.a7acec1b.chunk.js"
+  },
+  {
+    "revision": "b09b9199d786a207de11",
+    "url": "/TEMPURA/static/js/runtime-main.5f9b3620.js"
+  },
+  {
+    "revision": "19430c03077746303c317e4c77a6caef",
+    "url": "/TEMPURA/static/media/data_pipeline.19430c03.png"
+  },
+  {
+    "revision": "e5699354390d3bfa26d5346c2fa66727",
+    "url": "/TEMPURA/static/media/sft_data_example.e5699354.png"
+  },
+  {
+    "revision": "2b149475e50c535f0a120a7d165c116f",
+    "url": "/TEMPURA/static/media/teaser.2b149475.png"
+  },
+  {
+    "revision": "6dee30638a78997aaeebd7d79a8206e7",
+    "url": "/TEMPURA/static/media/training_pipeline.6dee3063.png"
+  }
+]);
